23dc3b2aac989eda4c9810103da4e03cc95b8cfc
Paul Nixon <pauljnixon@gmail.com>
2016-01-05 22:24:40 -0500

    Trying to guess what audio stream is active, via listening, via the Visualizer, including a workaround for the Android audio tunnel

--- src/net/nixonnet/audiofixermixer/MainActivity.java
+++ src/net/nixonnet/audiofixermixer/MainActivity.java
@@ -1,8 +1,11 @@
-package net.nixonnet.audiofixermixer;
+package net.nixonnet.musicvol;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.audiofx.Equalizer;
+import android.media.audiofx.Visualizer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -11,7 +14,8 @@ import android.widget.NumberPicker;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.CompoundButton;;
+import android.widget.CompoundButton;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -23,9 +27,13 @@ public class MainActivity extends Activity {
 	private TextView volDisp;
 	private Switch masterSwitch;
 	private Button scanButton;
+	private Button guessButton;
 	private Handler scanHandler;
 	private Runnable sessionIncrementer;
-	private boolean scanning;
+	private boolean foundSession;//via listening with the Visualizer
+	private boolean scanning;//with the scan button
+	private Handler guessHandler;
+	private MediaPlayer silentPlayer;//to prevent bypass
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +41,8 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		volSlider = (SeekBar) findViewById(R.id.seekBar1);
-		volSlider.setMax(20);
-		volSlider.setProgress(10);
+		volSlider.setMax(100);
+		volSlider.setProgress(25);
 		volSlider.setOnSeekBarChangeListener(new VolumeAdjuster());
 		
 		sessionPicker = (NumberPicker) findViewById(R.id.numberPicker1);
@@ -50,20 +58,42 @@ public class MainActivity extends Activity {
 		
 		scanHandler = new Handler();
 		sessionIncrementer = new SessionIncrementer();
-		
+
 		scanButton = (Button) findViewById(R.id.button1);
 		scanButton.setBackgroundColor(Color.LTGRAY);
 		scanButton.setOnClickListener(new SessionScannerListener());
+
+		guessHandler = new Handler();
+
+		guessButton = (Button) findViewById(R.id.button2);
+		guessButton.setBackgroundColor(Color.LTGRAY);
+		guessButton.setOnClickListener(new SessionGuesserListener());
+
+		silentPlayer = null;
+
+		Log.e("nixonnet.musicvol", "Successfully initialized listeners");
+	}
+
+	//taking this idea from https://github.com/felixpalmer/android-visualizer/commit/a0227b318b91451daf3fd8d23fb1a5c44a4e187d by h6ah4i
+	protected void onResume(){
+		super.onResume();
+		if(silentPlayer != null){
+			silentPlayer.release();
+		}
+		silentPlayer = MediaPlayer.create(getApplicationContext(), R.raw.silent);
+		silentPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 	}
 	
-	public void setVolume(short value){
+	public void setVolume(short value){//value is a percent
 		try{
-			//maximum is usually 200%
-			short level =  (short)(volumeLimits[0] + (short)((volumeLimits[1]-volumeLimits[0])*(value/40.0f)));
+			//maximum is usually 100%
+			short level =  (short)(volumeLimits[0] + (short)((volumeLimits[1]-volumeLimits[0])*(value/100.0f)));
+			Log.e("nixonnet.musicvol", "Setting volume to " + value + "% " + level + " of " + volumeLimits[1]);
 			eq.setEnabled(true);
 			short numbands = eq.getNumberOfBands();
 			for(short band = 0; band < numbands; band++){
-				short bandLevel = (short)(level+initialValues[band]);
+				//short bandLevel = (short)(level+initialValues[band]);
+				short bandLevel = level;
 				eq.setBandLevel(band, bandLevel);
 			}
 		}
@@ -73,6 +103,7 @@ public class MainActivity extends Activity {
 	}
 	
 	public void setAudioSession(int sessionNum){
+		Log.e("nixonnet.musicvol", "Setting session " + sessionNum);
 		try{
 			eq.release();
 		}catch(Exception e){}//if release fails then creation probably had also failed
@@ -97,7 +128,7 @@ public class MainActivity extends Activity {
 					eq.release();
 				} catch(Exception e){ }
 			} else {
-				setAudioSession(sessionPicker.getValue());
+				//setAudioSession(sessionPicker.getValue());
 			}
 		}
 	}
@@ -129,14 +160,92 @@ public class MainActivity extends Activity {
 			scanButton.setBackgroundColor(Color.LTGRAY);
 		}
 	}
-	
+
 	public class SessionScannerListener implements Button.OnClickListener {
 		@Override
 		public void onClick(View v) {
 			toggleScanState();
 		}
 	}
-	
+
+	public class SessionGuesserTask implements Runnable {
+		int sessionNum;
+		public SessionGuesserTask(int sessionNum){
+			this.sessionNum = sessionNum;
+			Log.e("nixonnet.musicvol","Created guesser for session " + sessionNum);
+		}
+		@Override
+		public void run() {
+			if(!foundSession && sessionNum < 100 && masterSwitch.isChecked()){
+				try {
+					//Log.e("nixonnet.musicvol", "About to guess for session " + sessionNum);
+					guessHandler.postDelayed(new SessionGuesserTask(sessionNum + 1), 1000);
+					Visualizer listener = new Visualizer(sessionNum);
+					listener.setEnabled(false);
+					listener.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+					int retval = listener.setDataCaptureListener(new VisualizerListener(sessionNum, listener), Visualizer.getMaxCaptureRate() / 2, true, false);
+					listener.setEnabled(true);
+					Log.e("nixonnet.musicvol", "Got " + retval + " setting listener on " + sessionNum);
+				} catch(Exception e){
+					//if this doesn't work it probably wasn't the right session
+					Log.e("nixonnet.musicvol", e.toString() + " from session " + sessionNum);
+				}
+			} else {//done
+				guessButton.setText("Guess!");
+				guessButton.setBackgroundColor(Color.LTGRAY);
+			}
+		}
+	}
+
+	public class SessionGuesserListener implements Button.OnClickListener {
+		@Override
+		public void onClick(View v) {
+			if(!masterSwitch.isChecked())return;
+			foundSession = false;
+			guessHandler.postDelayed(new SessionGuesserTask(1), 1000);
+			guessButton.setText("Thinking...");
+			guessButton.setBackgroundColor(Color.GREEN);
+		}
+	}
+
+	public class VisualizerListener implements Visualizer.OnDataCaptureListener {
+
+		private int listeningSession;
+		private Visualizer listeningVisualizer;
+
+		public VisualizerListener(int listeningSession, Visualizer listeningVisualizer){
+			//Log.e("nixonnet.musicvol", "Created visualizer listener for session " + listeningSession);
+			this.listeningSession = listeningSession;
+			this.listeningVisualizer = listeningVisualizer;
+		}
+
+		@Override
+		public void onWaveFormDataCapture(Visualizer visualizer, byte[] audioBuffer, int samplingRate) {
+			//Log.e("nixonnet.musicvol", "got data from session " + listeningSession);
+			if(!foundSession) {
+				int heardSomething = 0;
+				for (int i = 0; i < 16; i++) {
+					if (audioBuffer[i] != -128) heardSomething++;
+				}
+				if (heardSomething > 0) {
+					Log.e("nixonnet.musicvol", "heard something on session " + listeningSession);
+					foundSession = true;
+					setAudioSession(listeningSession);
+					sessionPicker.setValue(listeningSession);
+				} else {
+					Log.e("nixonnet.musicvol", "nothing on session " + listeningSession);
+				}
+				//remove self from listening
+				visualizer.release();
+			}
+		}
+
+		@Override
+		public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
+			//ignore
+		}
+	}
+
 	public class VolumeAdjuster implements SeekBar.OnSeekBarChangeListener {
 		@Override
 		public void onProgressChanged(SeekBar slider, int value, boolean fromUser) {
